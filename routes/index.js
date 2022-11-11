c8e1ec6ce9a22c9e7fba2d090d1758cf1bf7c77d
TechExpand <dailydevo9@gmail.com>
2022-11-11 13:21:19 -0800

    done

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,19 @@ const validateEmail = (email) => {
     );
   };
     
+
+  function makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() *
+        charactersLength));
+    }
+    return result;
+  }
+  
+
     
     function RemoveExtraSpace(value)
       {
