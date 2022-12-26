1c21c5d448e061f87f1da8593ada56aa65489ebf
TechExpand <dailydevo9@gmail.com>
2022-12-25 16:41:17 -0800

    new

--- routes/shipment.js
+++ routes/shipment.js
@@ -218,7 +218,7 @@ router.post("/shipment-price", async (req, res, next) => {
 		});
     const distance = Number(response.data.rows[0].elements[0].distance.text.toString().slice(response.data.rows[0].elements[0].distance.text.toString(), response.data.rows[0].elements[0].distance.text.toString().length-3))
     const distanceMiles =  Number(distance * 0.621)
-     const price = Math.ceil(Number(distanceMiles * 220));
+     const price = Math.ceil(Number(distanceMiles * 200));
      if(price>=3500){
       res.send({price:3000, distance: distanceMiles})
      }else{
